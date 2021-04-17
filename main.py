7c5cf924e1828938db7897b5a8471a592a1fe2be
unknown <kehindeibukun0619@gmail.com>
2021-04-17 22:45:30 +0100

    created the budget app

---
+++ main.py
@@ -0,0 +1,79 @@
+class Category:
+    def __init__(self, category):
+        self.ledger = []
+        self.amount = 0
+        self.category = category
+
+    def check_funds(self, amount):
+        if self.amount < amount:
+            return False
+        else:
+            return True
+
+    def deposit(self, amount, description=""):
+        self.ledger.append({"amount": amount, "description": description})
+        self.amount += amount
+
+    def withdraw(self, amount, description=""):
+        if self.check_funds(amount) == True:
+            self.amount -= amount
+            self.ledger.append({"amount": -amount, "description": description})
+            return True
+        else:
+            return False
+
+    # return balanace of budget
+    def get_balance(self):
+        return self.amount
+
+    def __str__(self):
+        result = ""
+        result += "----{}----".format(self.category)+"\n"
+        for transaction in self.ledger:
+            amount = 0
+            description = ""
+            for key, value in transaction.items():
+                if key == "amount":
+                    amount = value
+                elif key == "description":
+                    description = value
+            if len(description) > 23:
+                description = description[:23]
+            amount = str(format(float(amount), '.2f'))
+            if len(amount) > 7:
+                amount = amount[:7]
+            result += description + str(amount).rjust(30-len(description))+"\n"
+        result += "Total: "+str(format(float(self.amount), '.2f'))
+        return result
+
+    def transfer(self, amount, category):
+        if self.check_funds(amount) == True:
+            self.amount -= amount
+            self.ledger.append(
+                {"amount": -amount, "description": "Transfer to "+category.category})
+            category.ledger.append(
+                {"amount": amount, "description": "Transfer from "+self.category})
+            return True
+        else:
+            return False
+
+food = Category("Food")
+clothing = Category("Clothing")
+entertainment = Category("Entertainment")
+
+
+food.deposit(1000, "initial deposit")
+food.withdraw(10.15, "groceries")
+food.withdraw(15.89, "restaurant and more food for dessert")
+print(food.get_balance())
+
+food.transfer(50, clothing)
+clothing.withdraw(25.55)
+clothing.withdraw(100)
+
+entertainment.deposit(1000, "initial deposit")
+entertainment.withdraw(15)
+
+print(food)
+print(clothing)
+print(entertainment)
